4b47295f441d99bbe29f9f677fdabca929464ff3
MD IMRAN MADBAR <imranmadbar@gmail.com>
2018-03-22 18:28:32 +0600

    add data

--- PnP/src/main/java/com/imran/controller/BookDataTableServerSidePaginController.java
+++ PnP/src/main/java/com/imran/controller/BookDataTableServerSidePaginController.java
@@ -29,6 +29,9 @@ public class BookDataTableServerSidePaginController {
 	@Autowired
 	BookTwoService BookTwoService;
 	
+	
+	
+	
 	@Autowired
     BookDataTableServerSidePaginService bookDataTableServerSidePaginService;
 	
